c07e622d491d83e37f352b6bd31e02df1fb785f4
Mastermori <mastermoriii@gmail.com>
2019-04-14 21:32:09 +0200

    Initial commit

---
+++ src/de/mathlib/Vector2.java
@@ -0,0 +1,62 @@
+package de.mathlib;
+
+public class Vector2 {
+
+    public final float x, y;
+
+    public Vector2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float length(){
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    public Vector2 add(float x, float y) {
+        return new Vector2(this.x + x, this.y + y);
+    }
+    public Vector2 add(Vector2 vec) {
+        return new Vector2(this.x + vec.x, this.y + vec.y);
+    }
+
+    public Vector2 sub(float x, float y) {
+        return new Vector2(this.x - x, this.y - y);
+    }
+    public Vector2 sub(Vector2 vec) {
+        return new Vector2(this.x - vec.x, this.y - vec.y);
+    }
+
+    public Vector2 mult(float fac) {
+        return new Vector2(x * fac, y * fac);
+    }
+    public Vector2 mult(float xFac, float yFac) {
+        return new Vector2(x * xFac, y * yFac);
+    }
+    public Vector2 mult(Vector2 velFac) {
+        return new Vector2(x * velFac.x, y * velFac.y);
+    }
+
+    public Vector2 div(float div) {
+        return new Vector2(x / div, y / div);
+    }
+    public Vector2 div(float divX, float divY) {
+        return new Vector2(x / divX, y / divY);
+    }
+    public Vector2 div(Vector2 divVec) {
+        return new Vector2(x / divVec.x, y / divVec.y);
+    }
+
+    public Vector2 normalize() {
+        return div(length());
+    }
+
+    public float dot(Vector2 vec) {
+        return x * vec.x + y * vec.y;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Vector2(x, y);
+    }
+}
---
+++ src/de/mathlib/Vector3.java
@@ -0,0 +1,59 @@
+package de.mathlib;
+
+public class Vector3 {
+
+    public final float x, y, z;
+
+    public Vector3(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public float length(){
+        return (float) Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public Vector3 add(float x, float y, float z) {
+        return new Vector3(this.x + x, this.y + y, this.z + z);
+    }
+    public Vector3 add(Vector3 vec) {
+        return new Vector3(this.x + vec.x, this.y + vec.y, this.z + vec.z);
+    }
+
+    public Vector3 sub(float x, float y, float z) {
+        return new Vector3(this.x - x, this.y - y, this.z - z);
+    }
+    public Vector3 sub(Vector3 vec) {
+        return new Vector3(this.x - vec.x, this.y - vec.y, this.z - vec.z);
+    }
+
+    public Vector3 mult(float fac) {
+        return new Vector3(x * fac, y * fac, z * fac);
+    }
+    public Vector3 mult(float xFac, float yFac, float zFac) {
+        return new Vector3(x * xFac, y * yFac, z * zFac);
+    }
+    public Vector3 mult(Vector3 velFac) {
+        return new Vector3(x * velFac.x, y * velFac.y, z * velFac.z);
+    }
+
+    public Vector3 div(float div) {
+        return new Vector3(x / div, y / div, z / div);
+    }
+    public Vector3 div(float divX, float divY, float divZ) {
+        return new Vector3(x / divX, y / divY, z / divZ);
+    }
+    public Vector3 div(Vector3 divVec) {
+        return new Vector3(x / divVec.x, y / divVec.y, z / divVec.z);
+    }
+
+    public Vector3 normalize() {
+        return div(length());
+    }
+
+    public float dot(Vector3 vec) {
+        return x * vec.x + y * vec.y + z * vec.z;
+    }
+
+}
