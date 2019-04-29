7183daf4f2e552170196008d3e69b5a1127ad4c7
Mastermori <mastermoriii@gmail.com>
2019-04-29 16:27:18 +0200

    Added allTrue() for boolean arrays and listToArray() for List<Integer> to Utils class

--- src/de/mathlib/Utils.java
+++ src/de/mathlib/Utils.java
@@ -1,5 +1,7 @@
 package de.mathlib;
 
+import java.util.List;
+
 public class Utils {
 
     public static boolean binaryCheck(int b1, int b2) {
@@ -8,6 +10,24 @@ public class Utils {
 
     public static float clamp(float val, float min, float max) {
         return val < min ? min : val > max ? max : val;
+    }
+
+    public static boolean allTrue(boolean[] bools) {
+        for (boolean b : bools) {
+            if (!b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] listToArray(List<Integer> list){
+        int size = list.size();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
     }
 
 }
